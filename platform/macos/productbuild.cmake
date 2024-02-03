7c1c65c9d58642d3fd7f17962209efd5294f5535
Habtamu <habtamugirum0489@gmail.com>
2024-02-03 17:10:31 +0300

    launchDaemos

--- platform/macos/productbuild.cmake
+++ platform/macos/productbuild.cmake
@@ -1,14 +1,14 @@
 # platform/macos/productbuild.cmake
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
-set(CPACK_COMMAND_PRODUCTBUILD "${CMAKE_SOURCE_DIR}/productbuild.sh")
-set(CPACK_COMMAND_PKGBUILD "${CPACK_COMMAND_PRODUCTBUILD}")
+# set(CPACK_COMMAND_PRODUCTBUILD "${CMAKE_SOURCE_DIR}/productbuild.sh")
+# set(CPACK_COMMAND_PKGBUILD "${CPACK_COMMAND_PRODUCTBUILD}")
 
 install(
     FILES
         "${CMAKE_SOURCE_DIR}/io.osquery.agent.conf"
         "${CMAKE_SOURCE_DIR}/io.osquery.agent.plist"
     DESTINATION
-        "/private/var/osquery"
+        "/Library/LaunchDaemons"
     COMPONENT 
         vistar
 )
